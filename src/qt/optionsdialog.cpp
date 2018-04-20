ac11e822e670abf0b17b797be06b02573aedbcdd
Kirill Kirikov <cto@seductive.com.ua>
2018-04-20 19:06:30 +0300

    removed all themes except CIF-blue

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -93,10 +93,10 @@ OptionsDialog::OptionsDialog(QWidget *parent, bool enableWallet) :
     
     /* Theme selector */
     ui->theme->addItem(QString("CIF-blue"), QVariant("drkblue"));
-    ui->theme->addItem(QString("CIF-light"), QVariant("light"));
-    ui->theme->addItem(QString("CIF-light-hires"), QVariant("light-hires"));
-    ui->theme->addItem(QString("CIF-Crownium"), QVariant("crownium"));
-    ui->theme->addItem(QString("CIF-traditional"), QVariant("trad"));
+//    ui->theme->addItem(QString("CIF-light"), QVariant("light"));
+//    ui->theme->addItem(QString("CIF-light-hires"), QVariant("light-hires"));
+//    ui->theme->addItem(QString("CIF-Crownium"), QVariant("crownium"));
+//    ui->theme->addItem(QString("CIF-traditional"), QVariant("trad"));
     
     /* Language selector */
     QDir translations(":translations");

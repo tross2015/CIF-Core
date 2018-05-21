b2d51d49330e1311f99083c3e1229ae6baa6f8c2
Kirill Kirikov <cto@seductive.com.ua>
2018-05-21 19:55:18 +0300

    changed path to MacOSX sdk

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -1,6 +1,6 @@
 OSX_MIN_VERSION=10.7
 OSX_SDK_VERSION=10.13
-OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
+OSX_SDK=$(SDK_PATH)/MacOSX.sdk
 LD64_VERSION=241.9
 darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
 darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
